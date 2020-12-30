581c3005bed355e0db1b913c104cd70f4a01d609
Learn King <learnkingdemo@hotmail.com>
2020-12-30 20:51:01 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sampledotnetcore.Models;
+
+namespace sampledotnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        static List<EmployeeClass> employees = new List<EmployeeClass>()
+        {
+            new EmployeeClass{empID = "1",empName = "Muthu",empEmailID = "muthu@nokia.com",jobTitle = "Tech lead"},
+            new EmployeeClass{empID = "2",empName = "samy",empEmailID = "samy@nokia.com",jobTitle = "lead"},
+            new EmployeeClass{empID = "3",empName = "divya",empEmailID = "divya@nokia.com",jobTitle = "manager"}
+
+        };
+
+        [HttpGet]
+        public IEnumerable<EmployeeClass> GetEmployeesList()
+        {
+            return employees;
+        }
+
+        [HttpPost]
+        public void PostEmployeesList([FromBody]EmployeeClass objEmbloyee)
+        {
+            employees.Add(objEmbloyee);
+        }
+
+        [HttpPut("{empid}")]
+        public void PutEmployeesList1(int empid, [FromBody] EmployeeClass objEmbloyee)
+        {
+            employees[empid] = objEmbloyee;
+        }
+
+        [HttpDelete("{empid}")]
+        public void DeleteEmployeesList1(int empid)
+        {
+            employees.RemoveAt(empid);
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampledotnetcore.Models
+{
+    public class EmployeeClass
+    {
+        public string empID { get; set; }
+        public string empName { get; set; }
+        public string jobTitle { get; set; }
+        public string empEmailID { get; set; }
+    }
+}
